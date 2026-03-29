ac1c56f599a0fc79cc913c045c4a189b723ed665
Jakub Włodarski <kubawlodarski78@gmail.com>
2026-03-29 14:59:32 +0200

    remove unnecessary blank line in grade_3.0.py; add new file grade_5.0.py

--- Projekt-1/grade_3.0.py
+++ Projekt-1/grade_3.0.py
@@ -2,9 +2,8 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-# =========================
+
 # UKŁAD RÓWNAŃ (3 rząd)
-# =========================
 def dydt(X, t):
     y, yp, ypp = X   # y, y', y''
     
@@ -15,15 +14,12 @@ def dydt(X, t):
     return [dy_dt, dyp_dt, dypp_dt]
 
 
-# =========================
+
 # CZAS
-# =========================
 t = np.linspace(0, 10, 500)
 
 
-# =========================
 # WARUNKI POCZĄTKOWE
-# =========================
 y0 = 0
 y0p = 0
 y0pp = 0
@@ -31,26 +27,21 @@ y0pp = 0
 x0 = [y0, y0p, y0pp]
 
 
-# =========================
+
 # ROZWIĄZANIE ANALITYCZNE
-# =========================
 f = (1/9)*np.exp(-2*t) \
   - (1/4)*np.exp(-3*t) \
   + (5/36)*np.exp(-5*t) \
   + (1/6)*t*np.exp(-5*t)
 
 
-# =========================
 # ROZWIĄZANIE NUMERYCZNE
-# =========================
 solution = odeint(dydt, x0, t)
 
 print(solution[:10])
 
 
-# =========================
 # WYKRES
-# =========================
 plt.plot(t, solution[:,0], label="Rozwiązanie ODEINT")
 plt.plot(t, f, label="Rozwiązanie analityczne", linestyle='--')
 
